5178e1d61c90375cec4c8491d8cf801179a42b6f
Lucas Cantoni <76961476+lukcnt@users.noreply.github.com>
2023-03-01 23:26:32 -0300

    Create the basic template of the project

---
+++ components/sidebar.py
@@ -0,0 +1,11 @@
+import os
+import dash
+from dash import html, dcc
+from dash.dependencies import Input, Output, State
+import dash_bootstrap_components as dbc
+from app import app
+
+from datetime import datetime, date
+import plotly.express as px
+import numpy as np
+import pandas as pd
